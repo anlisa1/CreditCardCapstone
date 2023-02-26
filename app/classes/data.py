02904fbdd9cf423c7e9056a35040cc299d6ae509
anlisa1 <anlisa.liu2468@gmail.com>
2023-02-25 22:37:29 -0800

    html only

--- app/classes/data.py
+++ app/classes/data.py
@@ -50,6 +50,18 @@ class Blog(Document):
         'ordering': ['-createdate']
     }
 
+class Courses(Document):
+    author = ReferenceField('User',reverse_delete_rule=CASCADE) 
+    subject = StringField()
+    content = StringField()
+    tag = StringField()
+    tag2 = StringField()
+    create_date = DateTimeField(default=dt.datetime.utcnow)
+    modify_date = DateTimeField()
+    meta = {
+        'ordering': ['-createdate']
+    }
+
 class Comment(Document):
     # Line 63 is a way to access all the information in Course and Teacher w/o storing it in this class
     author = ReferenceField('User',reverse_delete_rule=CASCADE) 
