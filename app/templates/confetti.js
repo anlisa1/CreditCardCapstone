0f8d18156c181586d9184d97a4ce1fb0be935a22
anlisa1 <anlisa.liu2468@gmail.com>
2023-04-26 21:20:09 -0700

    for some reason this was added back?

--- app/templates/confetti.js
+++
@@ -1,8 +0,0 @@
-$(".confetti_button").click(function(){
-    $('.confetti_container').show();
-    setTimeout(function(){
-        $('.confetti_container').hide();
-    }, 8000);
-});
-
-// saidto put this as a script near html page
